a2990a55eb00cf18710a2ab1f9a49e1a1df3a6fe
rohitb9980 <rohitb9980@gmail.com>
2026-05-28 10:24:23 +0530

    Check CICD 3.0

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Kubernetes!');
+  res.end('Hello World from Rohit!');
 });
 
 server.listen(port, hostname, () => {
