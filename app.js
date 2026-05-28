95abb4c4fb3dedc80f876ec98d1d55c45432a050
rohitb9980 <rohitb9980@gmail.com>
2026-05-28 09:52:16 +0530

    Check CICD

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const { createServer } = require('node:http');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const hostname = '0.0.0.0';
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Kubernetes!');
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World from Kubernetes!');
 });
 
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
-  });
-}
-
-module.exports = app;
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
--- tests/app.test.ts
+++
@@ -1,6 +0,0 @@
-import { test } from 'node:test';
-import assert from 'node:assert';
-
-test('sample test', () => {
-  assert.strictEqual(1 + 1, 2);
-});
